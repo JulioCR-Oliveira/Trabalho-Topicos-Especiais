0233e634ea4c7e33d40b4e967090d34d63c35b0c
Júlio César R. de Oliveira <julioc.r.o@hotmail.com>
2022-10-10 15:08:46 -0300

    First commit,
    First commit,

---
+++ js/main.js
@@ -0,0 +1,27 @@
+'use strict'
+
+$("document").ready(function() {
+	/*let path = window.location.pathname;
+	let page = path.split("/").pop();
+	
+	switch (page) {
+		case "html.html":
+			
+			break;
+		case "css.html":
+			
+			break;
+		case "javascript.html":
+			
+			break;
+		case "jquery.html":
+			
+			break;
+		case "typescript.html":
+			
+			break;
+		default:
+			
+			break;
+	};*/
+});
