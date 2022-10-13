1da6f74d4af0e2d51217f6dc62314ca473a579eb
Júlio César R. de Oliveira <julioc.r.o@hotmail.com>
2022-10-13 11:11:03 -0300

    Aside e mudanças menores

--- js/_global-html.js
+++ js/_global-html.js
@@ -1,21 +1,52 @@
 $("document").ready(function() {
 	$("#global").html(`<header id="header">
 		<div id="logo">Trabalho de Tópicos Especiais</div>
-		<nav id="nav-bar">
-			<ul class="clearfix">
-				<li class="clearfix"><a href="index.html">Página inicial</a></li>
-				<li class="clearfix"><a href="*">html</a></li>
-				<li class="clearfix"><a href="*">css</a></li>
-				<li class="clearfix"><a href="*">JavaScript</a></li>
-				<li class="clearfix"><a href="*">JQuery</a></li>
-				<li class="clearfix"><a href="*">TypeScript</a></li>
+		<nav id="nav-bar" class="clearfix">
+			<ul>
+				<li><a href="index.html">Página inicial</a></li>
+				<li><a href="*">html</a></li>
+				<li><a href="*">css</a></li>
+				<li><a href="*">JavaScript</a></li>
+				<li><a href="*">JQuery</a></li>
+				<li><a href="*">TypeScript</a></li>
 			</ul>
 		</nav>
 	</header>
+
+	<section id="content" class="clearfix">
+		${createAside()}
+
+	</section>
+	`);			
 	
-	<section id="content">
-		
-	</section>`);
+	let content = $("#content");
+	let tempContent = $("#temp-content");
+	content.html(tempContent.html() + content.html());
+	tempContent.remove();
 	
 	$("footer").html("Trabalho de Tópicos Especiais, por Júlio César Rodrigues de Oliveira");
 });
+
+function createAside() {
+	return `
+		<aside>
+			<h3>Tópicos</h3>
+			<p>Tópico 1</p>
+			<p>Tópico 2</p>
+			<p>Tópico 3</p>
+			<p>Tópico 4</p>
+			<p>Tópico 5</p>
+			<p>Tópico 6</p>
+			<p>Tópico 7</p>
+			
+			<h3>Tópicos</h3>
+			<p>Tópico 1</p>
+			<p>Tópico 2</p>
+			<p>Tópico 3</p>
+			<p>Tópico 4</p>
+			<p>Tópico 5</p>
+			<p>Tópico 6</p>
+			<p>Tópico 7</p>
+		</aside>
+	`;
+};
